601515bbb75c7518a284982115861492bcb0af8a
Odrachael <missrachael41@gmail.com>
2024-03-06 12:50:26 -0800

    edited task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 """
-number of subscribers for a given subreddit
+function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers)for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
 """
-
-from requests import get
+import json
+import urllib.request
 
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
+    this function returns the number of subscribers for an account
+    0 if invalid.
     """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
     try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+        req = urllib.request.Request(url, headers=headers)
+        user_data = urllib.request.urlopen(req).read()
+        json_data = json.loads(user_data.decode('utf-8'))
+        subscribers = json_data['data']['subscribers']
+        return subscribers
+    except urllib.error.HTTPError as e:
+        if e.code == 404:
+            return 0
+        else:
+            print("Error:", e)
+            return 0
+    except Exception as e:
+        print("Error:", e)
         return 0
